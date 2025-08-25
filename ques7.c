13a85b7aaad3da869743967e28501978848d747d
Bhavya269-c <gulatibhavya25@gmail.com>
2025-08-25 20:07:13 +0530

    Add files via upload

---
+++ ques7.c
@@ -0,0 +1,18 @@
+//write a program to swap two numbers without using a third variable//
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter the value of two variable:");
+    scanf("%d %d",&a,&b);
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    printf("after swap:%d%d\n",a,b);
+
+    return 0;
+
+}
