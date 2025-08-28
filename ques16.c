2003adb49c22f5f3f2f0ee59b27c3b86e5c747c4
Bhavya269-c <gulatibhavya25@gmail.com>
2025-08-28 13:06:31 +0530

    Add files via upload

---
+++ ques16.c
@@ -0,0 +1,23 @@
+//write a program to input three numbers anf find the largest among them using if-else//
+
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+
+    printf("enter three numbers:");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a>=b &&a>=c){
+        printf("largest is %d\n",a);
+    }
+    else if(b>=a &&b>=c){
+        printf("largest is %d\n",b);
+    }
+    else{
+        printf("largest is %d\n",c);
+    }
+    
+    return 0;
+
+}
