35852e53ffa8057cfb4c4046f439956467185155
Bhavya269-c <gulatibhavya25@gmail.com>
2025-09-08 19:08:31 +0530

    Add files via upload

---
+++ ques38.c
@@ -0,0 +1,18 @@
+//write a program to find the sum of digits of a number//
+
+#include<stdio.h>
+int main(){
+    int num,sum=0,digit;
+
+    printf("enter the numbers:");
+    scanf("%d", &num);
+
+    while(num>0){
+        digit = num%10;
+        sum=sum+digit;
+        num=num/10;
+    }
+    printf("%d",sum);
+
+    return 0;
+}
