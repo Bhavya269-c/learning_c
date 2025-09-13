104176a859f2764a1674f815b19cb2f6cac01880
Bhavya269-c <gulatibhavya25@gmail.com>
2025-09-13 12:06:20 +0530

    Add files via upload

---
+++ ques48.c
@@ -0,0 +1,19 @@
+//write the program to print the following pattern//
+
+#include<stdio.h>
+int main(){
+    int i,j,n;
+
+    n=5;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("%d",j);
+
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+
+}
