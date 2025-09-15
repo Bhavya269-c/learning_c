c7f2e89ee4a51cade85cf73cecddae5404a9dd44
Bhavya269-c <gulatibhavya25@gmail.com>
2025-09-15 10:27:20 +0530

    Add files via upload

---
+++ ques52.c
@@ -0,0 +1,25 @@
+//write a program to print the following pattern://
+
+#include<stdio.h>
+
+int main(){
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+
+    return 0;
+}
