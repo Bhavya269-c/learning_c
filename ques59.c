3ece171830f6cb220e8342d971b9b1adf32d620b
Bhavya269-c <gulatibhavya25@gmail.com>
2025-09-19 23:41:32 +0530

    Add files via upload

---
+++ ques59.c
@@ -0,0 +1,27 @@
+//count even and odd numbers in an array//
+
+#include <stdio.h>
+
+int main() {
+    int n;
+   printf("enter the number n:");
+   scanf("%d", &n);  
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);  
+    }
+    
+    int evenCount = 0, oddCount = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+    
+    printf("Even=%d, Odd=%d\n", evenCount, oddCount);
+    return 0;
+}
+
+
