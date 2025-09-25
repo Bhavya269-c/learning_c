0fc16e9bc48f8463917e0dff5bf19f9137970dc9
Bhavya269-c <gulatibhavya25@gmail.com>
2025-09-25 09:35:29 +0530

    Add files via upload

---
+++ ques71.c
@@ -0,0 +1,27 @@
+//read and print matrix//
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+   
+   printf("enter the number of rows and cols:");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
