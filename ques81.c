2934af118bc5d393b3fdf3f00f3bab55877f5193
Bhavya269-c <gulatibhavya25@gmail.com>
2025-09-30 17:44:49 +0530

    Add files via upload

---
+++ ques81.c
@@ -0,0 +1,22 @@
+//count characters in a string without using built -in length functions//
+
+#include<stdio.h>
+
+int main(){
+    char str[1000];
+    int count = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    while (str[count] !='\0'){
+        count++;
+    }
+
+    if(count>0 &&str[count - 1]=='\n'){
+        count--;
+    }
+
+    printf("%d\n",count);
+
+    return 0;
+}
