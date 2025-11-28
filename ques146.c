68044af2b5e30a2da81d2aa0dbc5b279d07f435e
Bhavya269-c <gulatibhavya25@gmail.com>
2025-11-28 21:04:47 +0530

    Add files via upload

---
+++ ques146.c
@@ -0,0 +1,34 @@
+//Create Employee structure with nested Date structure for joining date and print details.//
+
+#include <stdio.h>
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+struct Employee {
+    char name[50];
+    int id;
+    struct Date joinDate;
+};
+
+int main() {
+    struct Employee e;
+
+    scanf("%s %d %d %d %d",
+          e.name, 
+          &e.id, 
+          &e.joinDate.day, 
+          &e.joinDate.month, 
+          &e.joinDate.year);
+
+    printf("Name: %s | ID: %d | Joining Date: %02d/%02d/%04d",
+           e.name, e.id,
+           e.joinDate.day,
+           e.joinDate.month,
+           e.joinDate.year);
+
+    return 0;
+}
