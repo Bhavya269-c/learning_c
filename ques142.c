4d6e5eedea0f17833fd6e96b0a639051d4c85215
Bhavya269-c <gulatibhavya25@gmail.com>
2025-11-28 21:01:17 +0530

    Add files via upload

---
+++ ques142.c
@@ -0,0 +1,24 @@
+//Store details of 5 students in an array of structures and print all.//
+
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s[5];
+    
+    for (int i = 0; i < 5; i++) {
+        scanf("%s %d %f", s[i].name, &s[i].roll, &s[i].marks);
+    }
+
+    printf("Name\tRoll\tMarks\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%s\t%d\t%.0f\n", s[i].name, s[i].roll, s[i].marks);
+    }
+
+    return 0;
+}
