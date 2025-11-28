34191de009f188f7c461664e3363a01be3171f99
Bhavya269-c <gulatibhavya25@gmail.com>
2025-11-28 21:02:56 +0530

    Add files via upload

---
+++ ques144.c
@@ -0,0 +1,23 @@
+//Write a function that accepts a structure as parameter and prints its members.//
+
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+void printStudent(struct Student s) {
+    printf("Name: %s | Roll: %d | Marks: %.0f", s.name, s.roll, s.marks);
+}
+
+int main() {
+    struct Student s;
+
+    scanf("%s %d %f", s.name, &s.roll, &s.marks);
+
+    printStudent(s);
+
+    return 0;
+}
